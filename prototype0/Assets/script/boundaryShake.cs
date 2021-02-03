ba84cdcb8645dc3fab2f0b9ebc9a008ada379082
zehui wu <zehuiwu@usc.edu>
2021-02-03 13:41:03 +0800

    final

---
+++ prototype0/Assets/script/boundaryShake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class boundaryShake : MonoBehaviour
+{
+    public float magnitude = 3f;
+    public counter count;
+    private void OnCollisionEnter2D()
+    {
+        CameraShaker.Instance.ShakeOnce(magnitude, 4f, .1f, 1f);
+        count.increaseScore();
+    }
+}
